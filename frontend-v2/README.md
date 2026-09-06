6ddb5a661171ad9aa59c810b9c39f683e757cfc7
Aboobaker Siddiq R <144536269+aboobakersiddiqr63@users.noreply.github.com>
2026-09-06 21:18:10 +0530

    Align remote develop with local frontend v2 files

--- frontend-v2/README.md
+++ frontend-v2/README.md
@@ -23,3 +23,4 @@ The current shell is deliberately small and data-free. Feature modules will be m
 npm install
 npm run dev
 ```
+
--- frontend-v2/src/main.tsx
+++ frontend-v2/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { PeriodFilterProvider } from './lib/periodFilter'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PeriodFilterProvider>
+      <App />
+    </PeriodFilterProvider>
   </StrictMode>,
 )
